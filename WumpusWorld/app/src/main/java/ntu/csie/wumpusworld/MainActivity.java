b3542ac094d03ce9a34c95d0b13bd797416a8e92
JCly-rikiu <jk123456789jp@gmail.com>
2016-06-18 13:37:33 +0800

    Create basic map

--- WumpusWorld/app/src/main/java/ntu/csie/wumpusworld/MainActivity.java
+++ WumpusWorld/app/src/main/java/ntu/csie/wumpusworld/MainActivity.java
@@ -1,7 +1,16 @@
 package ntu.csie.wumpusworld;
 
+import android.*;
+import android.Manifest;
+import android.app.Service;
+import android.content.pm.PackageManager;
+import android.graphics.PorterDuff;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,12 +36,12 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private String hostname;
 
     private enum Action {
-        RESUME, START, UPDATE, GO, SHOOT
+        INIT, RESUME, START, UPDATE, GO, SHOOT
     }
 
     private String nickname;
@@ -41,6 +50,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private TextView nicknameView;
 
     private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    private double latitude, longitude;
+    private final long LOCATION_UPDATE_MIN_TIME = 0;
+    private final float LOCATION_UPDATE_MIN_DISTANCE = 0;
+
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +69,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         homeView = findViewById(R.id.home);
 
         nicknameView = (TextView) findViewById(R.id.nickname);
+
+        mLocationManager = (LocationManager) this.getSystemService(Service.LOCATION_SERVICE);
+        requestLocationUpdates();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -98,16 +116,130 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Log.v("Location", latitude + " " + longitude);
+                return false;
+            }
+        });
+
+        init();
+        switchView(2);
+
+        LatLng ntu = new LatLng(25.0173405, 121.5397518);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ntu, 15));
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        changeLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private void requestLocationUpdates() {
+        boolean isGPSEnbaled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGPSEnbaled) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, this);
+            Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null)
+                changeLocation(location);
+        }
+    }
+
+    private void changeLocation(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Log.v("location", latitude + " " + longitude);
+    }
+
+    private void switchView(int viewnumber) {
+        switch (viewnumber) {
+            case 1:
+                progressView.setVisibility(View.VISIBLE);
+                gameView.setVisibility(View.GONE);
+                homeView.setVisibility(View.GONE);
+                break;
+            case 2:
+                progressView.setVisibility(View.GONE);
+                gameView.setVisibility(View.VISIBLE);
+                homeView.setVisibility(View.GONE);
+                break;
+            case 3:
+                progressView.setVisibility(View.GONE);
+                gameView.setVisibility(View.GONE);
+                homeView.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
+    private void init() {
+        new AsyncTask<Void, Void, JsonObject>() {
+            @Override
+            protected JsonObject doInBackground(Void... voids) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                                .url(hostname + "init")
+                                .build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    return new JsonParser().parse(response.body().string()).getAsJsonObject();
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(final JsonObject json) {
+                if (json == null) {
+                    finish();
+                    return;
+                }
+                int status = json.get("status").getAsInt();
+
+                switch (status) {
+                    case 0:
+                        break;
+                    case 1:
+                        finish();
+                        break;
+                }
+            }
+        }.execute();
     }
 
     private void resume() {
         setNickname();
 
         if (isNicknameValid(nickname)) {
+            switchView(1);
             new StartMenuTask(Action.RESUME).execute();
         } else {
             setNicknameViewError(getString(R.string.error_no_name));
@@ -118,6 +250,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setNickname();
 
         if (isNicknameValid(nickname)) {
+            switchView(1);
             new StartMenuTask(Action.START).execute();
         } else {
             setNicknameViewError(getString(R.string.error_no_name));
@@ -138,6 +271,18 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         return nickname.length() > 0;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (permissions.length == 1 && permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                }
+                return;
+            }
+        }
+    }
+
     public class StartMenuTask extends AsyncTask<Void, Void, JsonObject> {
 
         private final Action action;
@@ -148,6 +293,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         @Override
         protected JsonObject doInBackground(Void... params) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             OkHttpClient client = new OkHttpClient();
             Request request = null;
 
@@ -175,6 +326,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         @Override
         protected void onPostExecute(final JsonObject json) {
             if (json == null) {
+                switchView(3);
                 setNicknameViewError(getString(R.string.error_try_again));
                 return;
             }
@@ -184,7 +336,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 case 0:
                     break;
                 case 1:
-                    nicknameView.setError(getString(R.string.error_no_game));
+                    switchView(3);
+                    setNicknameViewError(getString(R.string.error_no_game));
                     break;
             }
         }
